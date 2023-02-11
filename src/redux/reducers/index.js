b91919b46e4cb63e264617879a8eb6de42a56a45
DaiNgo <daingo7211@gmail.com>
2023-02-11 12:39:20 +0700

    [languages]

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+import allPosts from "./allPosts";
+import post from "./post";
+import auth from "./auth";
+import categories from "./categories";
+import password from "./password";
+import noti from "./noti";
+export default combineReducers({
+    allPosts,
+    post,
+    auth,
+    categories,
+    password,
+    noti,
+});
